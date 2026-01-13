a1de67bdedc59f880bb047516b8bd99be11eefac
anelosima <none>
2026-01-13 13:19:16 -0500

    swap to gpt-5.2

--- main.py
+++ main.py
@@ -58,13 +58,12 @@ if __name__ == "__main__":
         skip_previously_forecasted_questions=True,
         extra_metadata_in_explanation=True,
         llms={
-            "default": RateLimitedLlm(  # settings should be from metac-o3
-                model="o3",  # o3 is the best reasoning LLM based on the metric of verified performance which is from a company offering API credits for use in the AI benchmark. The alternative would be the latest Gemini.
+            "default": RateLimitedLlm(
+                model="openai/gpt-5.2",
                 rate_limiter=AsyncLimiter(500),
-                temperature=1,
-                reasoning_effort="medium",
-                timeout=60 * 8,
-                allowed_tries=20,
+                reasoning_effort="high",
+                temperature=0.3,
+                timeout=15 * 60,  # settings should be from metac-gpt5.2-high
             ),
             "summarizer": "openai/gpt-4o-mini",
             "asknews_researcher": "asknews/news-summaries",
@@ -99,12 +98,14 @@ if __name__ == "__main__":
     elif run_mode == "test_questions":
         # Example questions are a good way to test the bot's performance on a single question
         EXAMPLE_QUESTIONS = [
-            "https://www.metaculus.com/questions/578/human-extinction-by-2100/",  # Human Extinction - Binary
-            "https://www.metaculus.com/questions/14333/age-of-oldest-human-as-of-2100/",  # Age of Oldest Human - Numeric
+            "https://www.metaculus.com/questions/41131/top-arc-agi-2-score-in-2026/",  # A question with many irrelevant news reports
+            # "https://www.metaculus.com/questions/578/human-extinction-by-2100/",  # Human Extinction - Binary
+            # "https://www.metaculus.com/questions/14333/age-of-oldest-human-as-of-2100/",  # Age of Oldest Human - Numeric
             # "https://www.metaculus.com/questions/22427/number-of-new-leading-ai-labs/",  # Number of New Leading AI Labs - Multiple Choice
             # "https://www.metaculus.com/c/diffusion-community/38880/how-many-us-labor-strikes-due-to-ai-in-2029/",  # Number of US Labor Strikes Due to AI in 2029 - Discrete
         ]
         minim_bot.skip_previously_forecasted_questions = False
+        minim_bot.publish_reports_to_metaculus = False
         questions = [
             client.get_question_by_url(question_url)
             for question_url in EXAMPLE_QUESTIONS
